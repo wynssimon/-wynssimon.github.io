44fdc746c7a630cf18bc21de28aa95947083f452
wynssimon <101094646+wynssimon@users.noreply.github.com>
2022-12-28 00:59:19 +0100

    eerste stappen

--- scripts/socails.js
+++
@@ -1,6 +0,0 @@
-const headerIFrame = document.querySelector('iframe[name="my-header"]');
-const headerLinks = headerIFrame.contentWindow.document.querySelectorAll("a");
-
-headerLinks.forEach((link) => {
-  link.href = link.href.replace("../", "/");
-});
